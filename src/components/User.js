512a8d41e9bea63ec162200f2f73986ccda6225f
tanka2u <ptanka123@gmail.com>
2024-04-02 03:00:02 +0200

    Using Class base Components

---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import classes from "./User.module.css";
+
+class User extends Component {
+  componentWillUnmount() {
+    console.log("Users are unmounted");
+  }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
